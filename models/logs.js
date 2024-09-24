82a8bd5c779454570dea12743be62427a0d15014
황승보 <ghkdtmdqh@naver.com>
2024-09-24 09:41:48 +0900

    TTL로직 추가

--- models/logs.js
+++ models/logs.js
@@ -3,4 +3,6 @@ const Schema = mongoose.Schema;
 
 const LogSchema = new Schema({});
 
+LogSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 * 60 * 24 * 3 });
+
 module.exports = mongoose.model('Logs', LogSchema);
